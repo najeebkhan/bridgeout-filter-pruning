d407ce42ab6ecd1e2d31a728513d02db0833f485
Najeeb Khan <najeeb.khan@usask.ca>
2020-05-30 12:17:33 -0600

    fixed input output dim of bo fc layer

---
+++ src/bridgeout_fc_std.py
@@ -0,0 +1,105 @@
+import math
+
+import torch
+from torch.nn.parameter import Parameter
+from torch.nn import functional as F
+from torch.nn import init
+from torch.nn import Module
+from sto_reg.src.sparseout import SO
+
+
+class Identity(Module):
+    r"""A placeholder identity operator that is argument-insensitive.
+    Args:
+        args: any argument (unused)
+        kwargs: any keyword argument (unused)
+    Examples::
+        >>> m = nn.Identity(54, unused_argument1=0.1, unused_argument2=False)
+        >>> input = torch.randn(128, 20)
+        >>> output = m(input)
+        >>> print(output.size())
+        torch.Size([128, 20])
+    """
+    def __init__(self, *args, **kwargs):
+        super(Identity, self).__init__()
+
+    def forward(self, input):
+        return input
+
+
+class BridgeoutFCLayer(Module):
+    r"""Applies a linear transformation to the incoming data: :math:`y = xA^T + b`
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        bias: If set to ``False``, the layer will not learn an additive bias.
+            Default: ``True``
+    Shape:
+        - Input: :math:`(N, *, H_{in})` where :math:`*` means any number of
+          additional dimensions and :math:`H_{in} = \text{in\_features}`
+        - Output: :math:`(N, *, H_{out})` where all but the last dimension
+          are the same shape as the input and :math:`H_{out} = \text{out\_features}`.
+    Attributes:
+        weight: the learnable weights of the module of shape
+            :math:`(\text{out\_features}, \text{in\_features})`. The values are
+            initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`, where
+            :math:`k = \frac{1}{\text{in\_features}}`
+        bias:   the learnable bias of the module of shape :math:`(\text{out\_features})`.
+                If :attr:`bias` is ``True``, the values are initialized from
+                :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})` where
+                :math:`k = \frac{1}{\text{in\_features}}`
+    Examples::
+        >>> m = nn.Linear(20, 30)
+        >>> input = torch.randn(128, 20)
+        >>> output = m(input)
+        >>> print(output.size())
+        torch.Size([128, 30])
+    """
+    __constants__ = ['in_features', 'out_features']
+
+    def __init__(
+             self,
+            in_features,
+            out_features,
+            p=0.5,
+            q=2.0,
+            target_fraction=1.0,
+            bias=True,
+            unit_test_mode=False):
+        super(BridgeoutFCLayer, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.Tensor(out_features, in_features))
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.unit_test_mode=unit_test_mode
+        self.p = p
+        self.q = q
+        self.tf=target_fraction
+        self.dropout = SO(self.p, self.q, self.tf)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input):
+        return F.linear(input, self.dropout(self.weight), self.bias)
+
+    def extra_repr(self):
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_features, self.out_features, self.bias is not None
+        )
+        
+        
+if __name__=='__main__':
+    m = BridgeoutFCLayer(20, 30)
+    input = torch.randn(128, 20)
+    output = m(input)
+    print(output.size())
+    
